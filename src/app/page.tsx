0b4e41c2a4e7454a5e2b17660aabee70a7ab0ab2
kavix <kavix34@gmail.com>
2025-11-11 10:33:08 +0000

    now completely delete this website whole things and only keep the hero s

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,5 @@
 import { Header } from '@/components/header';
 import { Hero } from '@/components/hero';
-import { About } from '@/components/about';
-import { Skills } from '@/components/skills';
-import { Projects } from '@/components/projects';
-import { Contact } from '@/components/contact';
-import { Footer } from '@/components/footer';
 
 export default function Home() {
   return (
@@ -12,12 +7,7 @@ export default function Home() {
       <Header />
       <main className="flex-1">
         <Hero />
-        <About />
-        <Skills />
-        <Projects />
-        <Contact />
       </main>
-      <Footer />
     </div>
   );
 }
