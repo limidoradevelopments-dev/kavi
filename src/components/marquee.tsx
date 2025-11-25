df547516a1bab7102017873e9125364a049a0fe0
kavix <kavix34@gmail.com>
2025-11-25 17:43:23 +0000

    marquee fixed

--- src/components/marquee.tsx
+++ src/components/marquee.tsx
@@ -1,32 +1,34 @@
-import Image from 'next/image';
+"use client"; // Needed only if you animate with JS
+
+import React from "react";
 
 const marqueeItems = [
-  'UI/UX DESIGN',
-  'WEB DESIGN',
-  'GRAPHIC DESIGN',
-  'UI/UX DESIGN',
-  'WEB DESIGN',
-  'GRAPHIC DESIGN',
+  "UI/UX DESIGN",
+  "WEB DESIGN",
+  "GRAPHIC DESIGN",
 ];
-
-const MarqueeItem = ({ text }: { text: string }) => (
-  <div className="inline-flex items-center gap-2.5 flex-shrink-0">
-    <Image width={31} height={29} alt="" src="/symbol-decoration.svg" />
-    <span className="font-headline text-mobile-h4 md:text-h5 text-white whitespace-nowrap">
-      {text}
-    </span>
-    <Image width={31} height={29} alt="" src="/symbol-decoration.svg" />
-  </div>
-);
 
 export function Marquee() {
   return (
     <section className="relative w-full overflow-hidden bg-black/90 py-3 lg:py-5 mt-4">
-      <div className="marquee-track">
+      {/* Marquee track */}
+      <div className="flex animate-marquee whitespace-nowrap">
         {[...marqueeItems, ...marqueeItems].map((text, index) => (
-          <MarqueeItem key={index} text={text} />
+          <div
+            key={index}
+            className="inline-flex items-center gap-2.5 flex-shrink-0 px-4"
+          >
+            {/* Decoration as background instead of multiple <Image> */}
+            <span className="w-8 h-8 bg-[url('/symbol-decoration.svg')] bg-contain bg-center bg-no-repeat" />
+            <span className="font-headline text-mobile-h4 md:text-h5 text-white whitespace-nowrap">
+              {text}
+            </span>
+            <span className="w-8 h-8 bg-[url('/symbol-decoration.svg')] bg-contain bg-center bg-no-repeat" />
+          </div>
         ))}
       </div>
+
+   
     </section>
   );
 }
