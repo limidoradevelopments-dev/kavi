64d0e055203efe022747b8e1119e8768a66ba8b2
kavix <kavix34@gmail.com>
2025-11-26 15:58:05 +0000

    windows

--- src/components/navigation-menu.tsx
+++ src/components/navigation-menu.tsx
@@ -33,15 +33,16 @@ export function NavigationMenu({ onClose }: { onClose: () => void }) {
       <div></div>
 
       {/* Center Navigation Links */}
-      <div className="flex-grow flex items-center justify-start">
-        <nav>
+      <div className=" flex lg:items-center items-start justify-start flex-col lg:flex-row gap-2 lg:gap-0 h-full overflow-scroll mt-20 lg:mt-0 ">
+        {/**navigation  */}
+        <nav className='flex-1'>
           <ul>
             {navItems.map((item, index) => (
               <li key={item.name} className="relative">
                 <Link
                   href={item.href}
                   onClick={(e) => handleLinkClick(e, item.href)}
-                  className="font-headline text-[3.5rem] md:text-[4.5rem] lg:text-[5.5rem] leading-none tracking-tighter hover:text-foreground transition-colors duration-300"
+                  className="font-headline text-mobile-h2 md:text-tablet-h2 lg:text-h2  leading-none tracking-tighter hover:text-foreground transition-colors duration-300"
                 >
                   {item.name}
                 </Link>
@@ -55,24 +56,40 @@ export function NavigationMenu({ onClose }: { onClose: () => void }) {
             ))}
           </ul>
         </nav>
-      </div>
 
-      {/* Bottom Information */}
-      <div className="w-full flex flex-col sm:flex-row justify-between items-end text-[10px] uppercase tracking-widest font-body">
-        <div className="mb-4 sm:mb-0">
-          <p>ALRIGHT RESERVED</p>
-          <p>2025</p>
+        {/**content side */}
+        <div className='flex h-full justify-end items-start md:items-end w-full lg:w-[35%] flex-1 text-[12px] tracking-widest flex-col gap-4'>
+        
+        <div className='flex flex-col lg:flex-row gap-0 lg:gap-[3rem] w-full justify-start items-start lg:justify-end lg:items-end'>
+            <div className='w-30% '>
+              <p>PERFECTION ISN'T ABOUT BEING FLAWLESS—IT'S</p>
+            </div>
+
+            <div className='w-full '>
+              <p>TIME GOES HERE</p>
+            </div>
         </div>
-        <div className="mb-4 sm:mb-0 text-center">
-            <p>D&D by Kavi</p>
-        </div>
-        <div className="text-right">
+        
+        <div className="text-left">
           <p>PERFECTION ISN'T ABOUT BEING FLAWLESS—IT'S</p>
           <p>ABOUT PUSHING IDEAS BEYOND LIMITS.</p>
           <p>YOUR NEXT IDEA CAN BE THE ONE.</p>
           <p>WHY WAIT?</p>
-          <p className="mt-2">CREATIVITY + NEXT-LEVEL VISION = PERFECTION</p>
+          <p className="mt-2 font-bold">CREATIVITY + NEXT-LEVEL VISION = PERFECTION</p>
         </div>
+        </div>
+      </div>
+
+      {/* Bottom Information */}
+      <div className="w-full lg:w-[50%] flex flex-row justify-between items-end text-[12px] uppercase tracking-widest font-body mt-2 lg:mt-0">
+        <div className="mb-4 sm:mb-0">
+          <p>ALRIGHT RESERVED 2025</p>
+        
+        </div>
+        <div className="mb-4 sm:mb-0 text-center">
+            <p>D&D by Kavi</p>
+        </div>
+      
       </div>
     </motion.div>
   );
