d2629fd9db855be98b31780076c980553c72c260
kavix <kavix34@gmail.com>
2025-12-12 07:12:46 +0000

    sss

--- src/components/marquee.tsx
+++ src/components/marquee.tsx
@@ -11,19 +11,19 @@ const marqueeItems = [
 
 const MarqueeItem = () => (
   <div className="flex-shrink-0 flex items-center gap-2.5 px-4">
-    <span className="w-8 h-8 bg-[url('/symbol-decoration.svg')] bg-contain bg-center bg-no-repeat" />
+    <span className="w-5 h-5 bg-[url('/symbol-decoration.svg')] bg-contain bg-center bg-no-repeat" />
     <span className="font-headline text-mobile-h4 md:text-h5 text-white whitespace-nowrap">
       {marqueeItems[0]}
     </span>
-    <span className="w-8 h-8 bg-[url('/symbol-decoration.svg')] bg-contain bg-center bg-no-repeat" />
+    <span className="w-5 h-5 bg-[url('/symbol-decoration.svg')] bg-contain bg-center bg-no-repeat" />
     <span className="font-headline text-mobile-h4 md:text-h5 text-white whitespace-nowrap ml-4">
       {marqueeItems[1]}
     </span>
-    <span className="w-8 h-8 bg-[url('/symbol-decoration.svg')] bg-contain bg-center bg-no-repeat ml-4" />
+    <span className="w-5 h-5 bg-[url('/symbol-decoration.svg')] bg-contain bg-center bg-no-repeat ml-4" />
     <span className="font-headline text-mobile-h4 md:text-h5 text-white whitespace-nowrap">
       {marqueeItems[2]}
     </span>
-    <span className="w-8 h-8 bg-[url('/symbol-decoration.svg')] bg-contain bg-center bg-no-repeat ml-4" />
+    <span className="w-5 h-5 bg-[url('/symbol-decoration.svg')] bg-contain bg-center bg-no-repeat ml-4" />
   </div>
 );
 
