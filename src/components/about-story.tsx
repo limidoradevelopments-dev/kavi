cf5402602fa28e0a16da72635c5df4a1c8db7b19
kavix <kavix34@gmail.com>
2025-12-27 10:48:30 +0000

    now this svg add to this border start point like that below attached ima

--- src/components/about-story.tsx
+++ src/components/about-story.tsx
@@ -3,6 +3,25 @@
 import React from 'react';
 import { AnimatedTitle } from './animated-title';
 
+const StarIcon = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <g style={{ mixBlendMode: 'overlay' }}>
+      <path
+        d="M24 11.7886C19.27 12.2186 16.6667 12.597 14.9983 13.9042C13.0375 15.435 12.5731 18.1071 12.0571 23.7143C11.5239 17.9007 11.0423 15.2458 8.89236 13.7494C7.22397 12.5798 4.52358 12.2014 0 11.7886C4.71278 11.3586 7.44757 10.963 9.09876 9.67304C11.0767 8.12505 11.5411 5.62437 12.0571 0C12.5387 5.17718 12.9687 7.66065 14.5683 9.26024C16.1679 10.8598 18.84 11.3242 24 11.7886Z"
+        fill="#424242"
+      />
+    </g>
+  </svg>
+);
+
+
 /* ---------------- Story Item ---------------- */
 const StoryItem = ({
   number,
@@ -66,8 +85,9 @@ export function AboutStory() {
 
           {/* Right Side */}
           <div className="md:col-span-5 md:col-start-8 flex flex-col gap-12 mt-8 md:mt-0">
-            <div className="relative pt-8">
-               <div className="absolute top-0 left-0 w-screen border-t border-foreground/20" />
+            <div className="relative pt-1">
+              <StarIcon className="absolute top-0 left-0 -translate-y-1/2 -translate-x-full" />
+              <div className="absolute top-0 left-0 w-screen border-t border-foreground/20" />
               <StoryItem number="001">
                 Perfection isn’t about being flawless — it’s about pushing ideas
                 beyond limits. Your next idea can be the one. Why wait?
@@ -76,7 +96,8 @@ export function AboutStory() {
           </div>
 
           <div className="md:col-span-4 md:col-start-5 flex flex-col gap-12 mt-8 md:mt-0">
-            <div className="relative pt-8">
+            <div className="relative pt-1">
+              <StarIcon className="absolute top-0 left-0 -translate-y-1/2 -translate-x-full" />
               <div className="absolute top-0 left-0 w-screen border-t border-foreground/20" />
               <StoryItem number="002">
                 Doing right by those who trust us isn&apos;t a strategy — it&apos;s
